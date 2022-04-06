2026128a731497ef75da0553cc6caef37131c432
nnanna <stanzicci@gmail.com>
2022-04-06 16:43:26 +0000

    my first commit

---
+++ app.java
@@ -0,0 +1,5 @@
+          echo "please enter your username"
+          read username
+          echo "please enter your password"
+          read -s password
+          echo "welcome to the zelle application"
